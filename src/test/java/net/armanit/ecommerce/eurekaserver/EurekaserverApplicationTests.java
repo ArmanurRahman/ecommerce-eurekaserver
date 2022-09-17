de6d05e712fd86e9b3adcabdefa7e367ff2ed369
Armanur Rahman <mubeenarman19@gmail.com>
2022-09-17 17:45:06 +0900

    initial commit

---
+++ src/test/java/net/armanit/ecommerce/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.armanit.ecommerce.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
